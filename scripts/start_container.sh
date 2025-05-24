f4872113532290f460d62fb4163756590081adef
suhail-12-10 <suhailahmedniyas45@gmail.com>
2025-05-24 13:22:40 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,5 +5,6 @@ set -e
 docker pull abhishekf5/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 abhishekf5/simple-python-flask-app  
+docker run -d -p 5000:5000 abhishekf5/simple-python-flask-app     
+
 
